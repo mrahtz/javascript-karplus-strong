363bd37571078849ca65324e2d68d847c7be6958
Matthew Rahtz <chiizufish@gmail.com>
2014-09-27 13:45:59 +0100

    Add basic dummy methods for guitar

---
+++ guitar.js
@@ -0,0 +1,36 @@
+function String(octave, semitone) {
+    this.basicHz = String.C0_HZ * Math.pow(2, octave+semitone/12);
+}
+
+// work from A0 as a reference,
+// since it has a nice round frequency
+String.A0_HZ = 27.5;
+// an increase in octave by 1 doubles the frequency
+// each octave is divided into 12 semitones
+// the scale goes C0, C0#, D0, D0#, E0, F0, F0#, G0, G0#, A0, A0#, B0
+// so go back 9 semitones to get to C0
+String.C0_HZ = String.A0_HZ * Math.pow(2, -9/12);
+
+String.prototype.pluck = function() {
+    console.log("Plucking string with frequency " + this.basicHz)
+}
+
+function Guitar() {
+    this.strings = [
+        new String(2, 4),   // E2
+        new String(2, 9),   // A2
+        new String(3, 2),   // D3
+        new String(3, 7),   // G3
+        new String(3, 11),  // B3
+        new String(4, 4)    // E4
+    ]
+}
+
+Guitar.prototype.pluckString = function(stringIndex) {
+    this.strings[stringIndex].pluck();
+}
+
+var guitar = new Guitar();
+for (i = 0; i < 6; i++) {
+    guitar.pluckString(i);
+}
