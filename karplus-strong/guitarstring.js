14c0352b03ad1fb7743dd719cfaba39fd0ead0a1
Matthew Rahtz <matthew.rahtz@gmail.com>
2015-03-03 20:48:42 +0000

    Refactor guitarstring.js, part 1

--- karplus-strong/guitarstring.js
+++ karplus-strong/guitarstring.js
@@ -3,22 +3,30 @@
 // it doesn't seem to appear in the ActionScript code anywhere...
 var timeUnit = 0.12;
 
-// === GuitarString ===
-
 function GuitarString(audioCtx, stringN, octave, semitone) {
     this.audioCtx = audioCtx;
-    this.basicHz = GuitarString.C0_HZ * Math.pow(2, octave+semitone/12);
+
+    // work from A0 as a reference,
+    // since it has a nice round frequency
+    var a0_hz = 27.5;
+    // an increase in octave by 1 doubles the frequency
+    // each octave is divided into 12 semitones
+    // the scale goes C0, C0#, D0, D0#, E0, F0, F0#, G0, G0#, A0, A0#, B0
+    // so go back 9 semitones to get to C0
+    var c0_hz = a0_hz * Math.pow(2, -9/12);
+    this.basicHz = c0_hz * Math.pow(2, octave+semitone/12);
     this.basicHz = this.basicHz.toFixed(2);
 
-    // this is only used in a magical calculation of filter coefficients
-    this.semitoneIndex = octave*12 + semitone - 9;
-    // also magic, used for stereo spread
-    // from -1 for first string to +1 for last
-    this.prePan = (stringN - 2.5) * 0.4;
-    
     var basicPeriod = 1/this.basicHz;
     var basicPeriodSamples = Math.round(basicPeriod * audioCtx.sampleRate);
     this.seedNoise = generateSeedNoise(65535, basicPeriodSamples);
+
+    // this is only used in a magical calculation of filter coefficients
+    this.semitoneIndex = octave*12 + semitone - 9;
+
+    // also magic, used for stereo spread
+    // from -1 for first string to +1 for last
+    this.prePan = (stringN - 2.5) * 0.4;
 
     function generateSeedNoise(seed, samples) {
         var noiseArray = new Float32Array(samples);
@@ -39,14 +47,6 @@ function GuitarString(audioCtx, stringN, octave, semitone) {
     }
 }
 
-// work from A0 as a reference,
-// since it has a nice round frequency
-GuitarString.A0_HZ = 27.5;
-// an increase in octave by 1 doubles the frequency
-// each octave is divided into 12 semitones
-// the scale goes C0, C0#, D0, D0#, E0, F0, F0#, G0, G0#, A0, A0#, B0
-// so go back 9 semitones to get to C0
-GuitarString.C0_HZ = GuitarString.A0_HZ * Math.pow(2, -9/12);
 
 GuitarString.prototype.pluck = function(time, velocity, tab) {
     console.log(this.basicHz + " Hz string being plucked" +
